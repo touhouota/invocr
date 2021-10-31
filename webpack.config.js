a4bd04029cca5815686241bf4b1a7e6fa0e1eccf
touhouota <touhouota@gmail.com>
2021-11-01 01:01:00 +0900

    最低限ビルドできるように

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 const path = require("path");
+const GasWebpackPlugin = require("gas-webpack-plugin");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -17,6 +18,7 @@ const config = {
   plugins: [
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    new GasWebpackPlugin()
   ],
   module: {
     rules: [
